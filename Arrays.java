62a67c01cd8497e2d2408ea4b82d11bd6a848522
Aniket Darje <aniketdarje9@gmail.com>
2026-05-21 22:43:41 +0530

    Array

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+class Arrays{
+
+    public static void main(String[] args) {
+        int arr[] = new int[4];
+        int arr1[] = {1,2,3,4};
+        for(int i=0; i<4;i++){
+            System.err.println(arr1[i]);
+        }
+    }
+}
