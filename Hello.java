ea351a4ff337e5eeed8cf4e0ef5c2fc2130dc66c
Aniket from Office <aniketdarje9@gmail.com>
2026-05-21 17:08:43 +0530

    hello

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello{
+    public static void main(String[] args) {
+        System.err.println("Hello World!");
+
+        // Type Casting (Explicit Conversion)
+        int a =100;
+        byte b= (byte) a;
+        System.err.println(b); 
+    }
+}  
