a9c9336ecbaade592a05eefd8b2019946020b18c
Aniket from Office <aniketdarje9@gmail.com>
2026-06-10 19:21:48 +0530

    .

--- Array1.java
+++ Array1.java
@@ -113,21 +113,43 @@ public class Array1 {
         // System.out.println(isSorted);
 
 
-        // To move all zeroes to end
-        int nums[]={1,0,2,0,3,0,4};
-       for(int i=0;i<nums.length-1; i++){
-        for(int j=i+1; j<nums.length; j++)
-        if(nums[i]==0){
-            int temp =nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
-       }
+        // To move all zeroes to end O(n^2)
+//         int nums[]={1,0,2,0,3,0,4};
+//        for(int i=0;i<nums.length-1; i++){
+//         for(int j=i+1; j<nums.length; j++)
+//         if(nums[i]==0){
+//             int temp =nums[i];
+//             nums[i] = nums[j];
+//             nums[j] = temp;
+//         }
+//        }
 
-// System.out.println();
-for(int num : nums){
-System.out.println(num);
-}
+// // System.out.println();
+// for(int num : nums){
+// System.out.println(num);
+// }
+
+// Solution 2 with time complexity O(n)
+
+// int nums[] = {1,0,2,0,3,0,4};
+
+// int j= 0;
+
+// for(int i=0; i<nums.length;i++){
+//     if(nums[i]!=0){
+//         nums[j]= nums[i];
+//         j++;
+//     }
+// }
+
+// while(j<nums.length){
+//     nums[j]=0;
+//     j++;
+// }
+// for(int num: nums){
+//     System.out.println(num);
+// }
+
 
     }
     
---
+++ Array2.java
@@ -0,0 +1,83 @@
+public class Array2 {
+
+    public static void main(String[] args) {
+        
+        // Problem 11: Remove Duplicates from Sorted Array
+        // This gives I think n^3 time complexity
+//         int arr[] = {1,1,2,2,3,3,4,5,5,6,7};
+// int size =arr.length;
+        // for(int i=0; i<size;i++){
+        //     for(int j=i+1; j<size; j++){
+        //         if(arr[i]==arr[j]){
+        //           for(int k =j; k<size-1; k++){
+        //             arr[k]= arr[k+1];
+        //           }
+        //             size--;
+        //             j--;
+        //         for(int x=0; x<size; x++){
+        //             System.out.print(arr[x]);
+                   
+        //         } System.out.println("");
+        //         }
+                
+        //     }
+        // }
+        // for(int i=0; i<size; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+
+        // Let's try optimal solution of O(n)
+         int arr[] = {1,1,2,2,3,3,4,5,5,6,7};
+         int size =arr.length;
+         int j=0; 
+        //  j is just a pointer that may 
+
+        for(int i=1; i<size; i++){
+
+            if(arr[j]!=arr[i]){
+                j++;
+                arr[j] =arr[i];
+            }
+
+        }
+        for(int x=0; x<=j; x++){
+            System.out.println(arr[x]);
+        }
+
+    }
+}
+
+
+
+// public class Array2 {
+
+//     public static void main(String[] args) {
+
+//         int[] arr = {1,1,2,2,3,3,4,5,5};
+
+//         int size = arr.length;
+
+//         for (int i = 0; i < size; i++) {
+
+//             for (int j = i + 1; j < size; j++) {
+
+//                 if (arr[i] == arr[j]) {
+
+//                     // Shift elements left
+//                     for (int k = j; k < size - 1; k++) {
+//                         arr[k] = arr[k + 1];
+//                     }
+
+//                     size--;
+
+//                     j--; // Check the new element that shifted into position j
+//                 }
+//             }
+//         }
+
+//         for (int i = 0; i < size; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
